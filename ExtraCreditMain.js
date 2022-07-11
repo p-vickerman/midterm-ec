8e0849af6ba34f947f7d09420d0a0e059e705f97
p-vickerman <patrick@vickerman.net>
2022-07-11 08:26:52 -0400

    adding main.js

---
+++ ExtraCreditMain.js
@@ -0,0 +1,23 @@
+    function twoSmallest(array){
+    
+        if (array.length < 2){
+            console.log(undefined)
+        }
+        let secondSmallest = Math.pow(200, 8)
+        let smallest = Math.pow(200, 8)
+    
+        for (let i = 0; i < array.length; i++){
+            let current = array[i];
+            if(smallest>current){
+                secondSmallest=smallest;
+                smallest=current;
+            
+            }else if (array[i] > smallest && array[i] < secondSmallest){
+
+            }
+           
+        }
+        return [smallest, secondSmallest]
+    }
+let arr = [ 12, 13, 1, 10, 34, 1 ]; 
+console.log(twoSmallest(arr));
